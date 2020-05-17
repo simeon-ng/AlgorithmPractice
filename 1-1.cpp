b1f404edf01ac11216190dc371c6840831986874
Simeon Ng <ssng@alaska.edu>
2020-05-17 03:40:56 -0800

    Add 1-1

---
+++ 1-1.cpp
@@ -0,0 +1,14 @@
+// 1-1
+// Is Unique: Implement an algorithm to determine if a string has all unique characters. 
+// What if you cannot use additional data structures?
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+
+    cout << "hello" << endl;
+
+    return 0;
+}
