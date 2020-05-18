59637863866bb14959f149887ccc5b5faed64271
Simeon Ng <ssng@alaska.edu>
2020-05-17 16:25:06 -0800

    Hash table solution 1-1

--- 1-1.cpp
+++ 1-1.cpp
@@ -1,14 +1,35 @@
-// 1-1
+// Simeon Ng
+// 5/16/20
+// Problem 1-1
 // Is Unique: Implement an algorithm to determine if a string has all unique characters. 
 // What if you cannot use additional data structures?
 
 #include <iostream>
-using std::cout;
-using std::endl;
+#include <unordered_map>
+#include <string>
+using namespace std;
 
-int main() {
+int main() 
+{
+    string testStr;
+    cin >> testStr;
 
-    cout << "hello" << endl;
+    unordered_map<char, int> umap;
+    bool isUnique = true;
+    for (int i = 0; i < testStr.size(); i++)
+    {
+        auto search = umap.find(testStr[i]);
+        if (search != umap.end()) {
+            isUnique = false;
+            break;
+        }
+        umap[testStr[i]] = i;
+    }
 
+    if (isUnique)
+        cout << testStr << " is unique" << endl;
+    else
+        cout << testStr << " is not unique" << endl;
+    
     return 0;
 }
